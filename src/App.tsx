bc25d0587fc4e671b2ad32452674b3f8cbc56781
unknown <muh.it.eng.19841984@gmail.com>
2022-10-13 13:26:43 +0300

    add roles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import UserCreate from "./pages/users/UserCreate";
 import UserEdit from "./pages/users/UserEdit";
 import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
+import Roles from "./pages/roles/Roles";
+import RoleCreate from "./pages/roles/RoleCreate";
+import RoleEdit from "./pages/roles/RoleEdit";
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
           <Route path={"/users"} element={<Users />} />
           <Route path={"/users/create"} element={<UserCreate />} />
           <Route path={"/users/:id/edit"} element={<UserEdit />} />
+          <Route path={'/roles'}  element={<Roles/>}/>
+                <Route path={'/roles/create'}  element={<RoleCreate/>}/>
+                <Route path={'/roles/:id/edit'}  element={<RoleEdit/>}/>
         </Routes>
       </BrowserRouter>
     </div>
