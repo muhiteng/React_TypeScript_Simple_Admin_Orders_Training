faa48b95c894152f55d0e0e353d18e7d02253ec1
unknown <muh.it.eng.19841984@gmail.com>
2022-10-13 13:22:41 +0300

    add user routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-
-import "./App.css";
+import Users from "./pages/users/Users";
+import UserCreate from "./pages/users/UserCreate";
+import UserEdit from "./pages/users/UserEdit";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path={"/profile"} element={<Profile />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path={"/users"} element={<Users />} />
+          <Route path={"/users/create"} element={<UserCreate />} />
+          <Route path={"/users/:id/edit"} element={<UserEdit />} />
         </Routes>
       </BrowserRouter>
     </div>
